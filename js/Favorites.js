39ff593ae4c45fe5b426e6423dd37a476ec3ba85
Pedro Henrique  Fernandes Silva <pedrosilva@INF122-LNX.iec.local>
2024-04-25 16:34:16 -0300

    adding new features

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,22 +1,52 @@
+import { GihubUser } from "./GithubUser.js"
+
 export class Favorites {
     constructor(root) {
         this.root = document.querySelector(root)
         this.load()
+
+        GihubUser.search('diego3g').then(user => console.log(user))
     }
 
     load() {
-        this.entries = [{
-            login: 'phfernandes-dev',
-            name: 'Pedro Fernandes',
-            public_repos: '76',
-            followers: '12000'
-        },
-        {
-            login: 'diego3g',
-            name: 'Diego Fernandes',
-            public_repos: '76',
-            followers: '12000'
-        }]
+
+        this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+    }
+
+    save(){
+        localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
+    }
+
+    async add(username){
+        try {
+
+            const userExists = this.entries.find(entry => entry.login === username)
+
+            if(userExists){
+                throw new Error ('Usuário já favoritado')
+            }
+
+            const user = await GihubUser.search(username)
+
+            if(user.login === undefined) {
+                throw new Error('Usuário não encontrado!')
+            }
+
+            this.entries = [user, ...this.entries]
+            this.update()
+            this.save()
+
+        } catch(error) {
+        alert(error.message)
+        }
+    }
+
+    delete(user) {
+        const filteredEntries = this.entries.filter(entry => entry.login !== user.login)
+
+        this.entries = filteredEntries
+        this.update()
+        this.save()
     }
 }
 
@@ -28,6 +58,16 @@ export class FavoritesView extends Favorites {
 
 
         this.update()
+        this.onadd()
+    }
+
+    onadd(){
+        const addButton = this.root.querySelector('.search button')
+        addButton.onclick = () => {
+            const { value } = this.root.querySelector('.search input')
+
+            this.add(value)
+        }
     }
 
     update() {
@@ -38,10 +78,18 @@ export class FavoritesView extends Favorites {
 
             row.querySelector('.user img').src = `https://github.com/${user.login}.png`
             row.querySelector('.user img').alt = `Imagem de ${user.name}`
+            row.querySelector('.user a').href = `https://github.com/${user.login}`
             row.querySelector('.user p').textContent = user.name
             row.querySelector('.user span').textContent = user.login
             row.querySelector('.repos').textContent = user.public_repos
             row.querySelector('.followers').textContent = user.followers
+
+            row.querySelector('.remove').onclick = () => {
+                const isOk = confirm('Tem certeza que deseja deletar esta linha?')
+                if(isOk) {
+                    this.delete(user)
+                }
+            }
 
             this.tbody.append(row)
         })
